1b19d326567f5d820aaa77e871584007b8694869
Saadiya-23 <145015530+Saadiya-23@users.noreply.github.com>
2025-07-25 15:01:11 +0530

    Update practice.kt

--- practice.kt
+++ practice.kt
@@ -5,16 +5,26 @@ fun main() {
     for(i in 0..49){
         arr[i]=Random.nextInt(1,1001)
     }
-
+    print("Initial Array:")
     for(i in 0..49){
+
         print("${arr[i]} ")
     }
     println()
     println(arr.size)
     val list=arr.toMutableList()
+    val randArr=mutableListOf<Int>()
     for(i in 0..24){
-        list.removeAt(Random.nextInt(0,list.size))
+        val num=Random.nextInt(0,50)
+        list[num]=-1
+        randArr.add(num)
+
     }
-    println(list)
-    println(list.size)
+    val res=mutableListOf<Int>()
+    for(num in list){
+        if(num!=-1) res.add(num)
+    }
+    println("After deletion : ${res}")
+    println(res.size)
+    println("Deleted Index: ${randArr.sorted()}")
 }
